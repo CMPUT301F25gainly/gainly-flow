d4dd681e81a84280599650e90ed17364f7495440
Ilhamoddin Arbabzada <112998037+ilhamoddin@users.noreply.github.com>
2025-11-06 00:52:01 -0700

    Added Firestore and added event poster functionality

--- Gainly_Flow/app/src/main/java/com/example/gainly_flow/MainActivity.java
+++ Gainly_Flow/app/src/main/java/com/example/gainly_flow/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 //                startActivity(new Intent(this, EntrantViewMain.class)));
 //
         organizer.setOnClickListener(v ->
-                startActivity(new Intent(this, OrganizerLanding.class)));
+                startActivity(new Intent(this, CreateEvent.class)));
 
         adminLogin.setOnClickListener(v ->
                 startActivity(new Intent(this, AdminMainActivity.class)));
