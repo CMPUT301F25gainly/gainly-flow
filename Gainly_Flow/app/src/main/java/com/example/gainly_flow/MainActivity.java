8d1895c4d16ee4c1ce6c0e69b039e93163ef190b
hxuoc <xchou9458@gmail.com>
2025-11-06 13:44:46 -0700

    solve some minor errors

--- Gainly_Flow/app/src/main/java/com/example/gainly_flow/MainActivity.java
+++ Gainly_Flow/app/src/main/java/com/example/gainly_flow/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.gainly_flow.AdminMainActivity;
 
@@ -14,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
 
 //         Button organizerViewButton = findViewById(R.id.organizerButton);
 //         Button adminViewButton = findViewById(R.id.adminButton);
