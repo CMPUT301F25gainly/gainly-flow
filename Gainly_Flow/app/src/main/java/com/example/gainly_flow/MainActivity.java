3e775aa0d0fbacdc5dc98b2748397048f23cd33e
hxuoc <xchou9458@gmail.com>
2025-11-05 22:42:56 -0700

    solve some minor errors after merge

--- Gainly_Flow/app/src/main/java/com/example/gainly_flow/MainActivity.java
+++ Gainly_Flow/app/src/main/java/com/example/gainly_flow/MainActivity.java
@@ -14,11 +14,11 @@ public class MainActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
 
 //         Button organizerViewButton = findViewById(R.id.organizerButton);
 //         Button adminViewButton = findViewById(R.id.adminButton);
