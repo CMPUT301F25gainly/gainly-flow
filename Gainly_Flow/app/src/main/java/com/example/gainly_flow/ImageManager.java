093eb831504d19e96abe3893130ac4e20d757708
eshaankrishna <eshaankr@ualberta.ca>
2025-11-07 19:20:15 +0300

    Remove old Gainly_Flow

--- Gainly_Flow/app/src/main/java/com/example/gainly_flow/ImageManager.java
+++
@@ -1,8 +0,0 @@
-package com.example.gainly_flow;
-
-public class ImageManager {
-    public String uploadPoster(byte[] imageBytes) { return null; }
-    public void updatePoster(String imageId, byte[] imageBytes) {}
-    public void deleteImage(String imageId) {}
-    public byte[] fetchImage(String imageId) { return null; }
-}
--- Gainly_Flow/app/src/main/java/com/example/gainly_flow/OrganizerViewMain.java
+++
@@ -1,6 +0,0 @@
-package com.example.gainly_flow;
-
-import android.app.Activity;
-
-public class OrganizerViewMain extends Activity {
-}
